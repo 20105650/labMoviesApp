fcbcf18d95eda1ae887142e58d08e8f077b1a40b
20105650 <20105650@mail.wit.ie>
2024-04-03 12:45:15 +0100

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { FC, useState, useEffect } from 'react';
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
@@ -6,6 +6,8 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
 import { MovieT } from "../../types/interfaces"; 
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Avatar from "@mui/material/Avatar";
 
 const styles = {
     root: {  
@@ -15,17 +17,36 @@ const styles = {
     flexWrap: "wrap",
     padding: 1.5,
   },
+  avatar: {
+    backgroundColor: "rgb(255, 0, 0)",
+  },
 };
 
-const MovieHeader: React.FC<MovieT> = (props) => {
-  
+const MovieHeader: FC<MovieT> = (props) => {
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Retrieve movies from localStorage
+    const movies = JSON.parse(localStorage.getItem("favourites") || '[]');
+
+    // Check if the movie with the given id exists in favorites
+    const favorite = movies.find((movie: any) => movie.id === props.id);
+    
+    // Update isFavorite state based on whether the movie is in favorites
+    setIsFavorite(!!favorite);
+  }, [props.id]);
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
-      <Typography variant="h4" component="h3">
+      <Typography variant="h4" component="h3"> 
+      {isFavorite && (
+        <Avatar sx={{ backgroundColor: 'red' }}>
+        <FavoriteIcon fontSize="large" />
+      </Avatar>
+      )}
         {props.title}{"   "}
         <a href={props.homepage}>
           <HomeIcon color="primary"  fontSize="large"/>
